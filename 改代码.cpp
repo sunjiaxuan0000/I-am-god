b2612b3ea45b7c31021d2d8b77602ff7dff2c7bc
sunjiaxuan0000 <bdlfy_0902@qq.com>
2025-11-17 19:23:35 +0800

    Update 改代码.cpp

--- 改代码.cpp
+++ 改代码.cpp
@@ -1,33 +1,12 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
-vector<int> factors;
-void findperfectnumber(int a) {
-	int b = 0;
-	for (int i = 1; i < a / 2+1; i++) {
-		if (a % i == 0) {
-			factors.push_back(i);
-			b += i;
-		}
-		if (a == b) {
-			cout << a << "its factors are";
-			for (int x : factors) {
-				cout << x << "";
-			}
-			cout << endl;
-		}
-		else{
-			return;
-		}
-	}
-}
-int main() {
-	int N;
-	cin >> N;
-	for (int i = 2; i <= N; i++) {
-		factors.clear();
-		findperfectnumber(i);
-	}
-	system("pause");
-		return 0;
+int main()
+{
+	int k=0;
+	int i = k + 1;
+	cout << i++ << endl;
+	int i = 1;
+	cout << i++ << endl;
+	cout << "Welcome to C++"<<endl;
+	return 0;
 }
